0daec6a48b3cfd49bd852b810743d7d0b6e6b21f
Vladimir Alexandrov <dev.alexandrov@gmail.com>
2018-06-26 17:57:29 +0300

    bags index page

--- app/js/common.js
+++ app/js/common.js
@@ -3,28 +3,84 @@
         arrows: false,
         dots: false,
         infinite: true,
-        speed: 300,
+        speed: 600,
         slidesToShow: 16,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         responsive: [
             {
-                breakpoint: 1800,
+                breakpoint: 2401,
+                settings: {
+                    slidesToShow: 21,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 2000,
+                settings: {
+                    slidesToShow: 18,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1801,
+                settings: {
+                    slidesToShow: 16,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1401,
                 settings: {
                     slidesToShow: 12,
-                    slidesToScroll: 2
+                    slidesToScroll: 1
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 1201,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 10,
+                    slidesToScroll: 1
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 992,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 10,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 901,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 701,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 551,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 451,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 351,
+                settings: {
+                    slidesToShow: 3,
                     slidesToScroll: 1
                 }
             }
